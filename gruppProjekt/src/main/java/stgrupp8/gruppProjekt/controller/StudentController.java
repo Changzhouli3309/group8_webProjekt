44e10297353f850aa5c783740f02712586beb2e1
Changzhou Li <45784976+Changzhouli3309@users.noreply.github.com>
2021-04-15 18:16:44 +0200

    ver1.02

--- gruppProjekt/src/main/java/stgrupp8/gruppProjekt/controller/StudentController.java
+++ gruppProjekt/src/main/java/stgrupp8/gruppProjekt/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import stgrupp8.gruppProjekt.service.StudentService;
 
 @RestController
 @RequestMapping("/api") // http://localhost:8080/api/student
+@CrossOrigin(origins = "http://localhost:3000")
 public class StudentController {
 
 	private StudentService stu_ser;
